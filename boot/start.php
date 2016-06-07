c3c2690b1f042b711e954d1b9ad624a5878baf11
Wallace de Souza Vizerra <wallacemaxters@gmail.com>
2016-06-07 14:59:37 -0300

    Agora a parte das rotas foi movida par ao provedor de serviço

--- boot/start.php
+++ boot/start.php
@@ -8,48 +8,8 @@ define('WEB_PATH', ROOT_PATH . '/app/');
 
 include_once ROOT_PATH . '/vendor/autoload.php';
 
-$app = new Maxters\Container;
+$app = new Maxters\Container();
 
-// Define container Classes
-
-$app['debug'] = true;
-
-$app->singleton('router', new \PHPLegends\Routes\Router);
-
-$app['view'] = function ($name, $data) {
-
-	$path = APP_PATH . '/Views/';
-
-	return new \PHPLegends\View\View($name, $data, $path, 'phtml');
-};
-
-$app->singleton('request', PHPLegends\Http\ServerRequest::createFromGlobals());
-
-$app['response'] = function ($message, $code, array $headers = [])
-{
-	return new PHPLegends\Http\Response($message, $code, $headers);
-};
-
-$app['response.view'] = function ($name, array $data = [], $code = 200, array $headers = []) use($app)
-{
-	return $app['response']($app['view']($name, $data)->render(), $code, $headers);
-};
-
-$app['response.json'] = function ($data, $code = 200, array $headers = [])
-{
-	return new PHPLegends\Http\JsonResponse($data, $code, 0, $headers);
-};
-
-$app['response.redirect'] = function ($to, $code = 200, array $headers = [])
-{
-	return new PHPLegends\Http\RedirectResponse($to, $code, $headers);
-};
-
-call_user_func(function () use ($app) {
-
-	$router = $app['router'];
-
-	include_once APP_PATH . '/app.php';
-});
+$app->register(new Maxters\Providers\AppProvider($app));
 
 return $app;

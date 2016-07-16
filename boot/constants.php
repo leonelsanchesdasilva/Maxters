892e69f3e9f3e12661259d866761f9c98a40a210
Wallace de Souza <wallacemaxters@gmail.com>
2016-07-16 19:06:03 -0300

    melhorando a estrutura

---
+++ boot/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+define('ROOT_PATH', dirname(__DIR__));
+
+define('APP_PATH', ROOT_PATH . '/app');
+
+define('RESOURCES_PATH', ROOT_PATH . '/resources');
+
+define('WEB_PATH', ROOT_PATH . '/web'); 
+
+define('DB_PATH', ROOT_PATH . '/db'); 
--- boot/start.php
+++ boot/start.php
@@ -1,14 +1,6 @@
 <?php
 
-define('ROOT_PATH', dirname(__DIR__));
-
-define('APP_PATH', ROOT_PATH . '/app');
-
-define('RESOURCES_PATH', ROOT_PATH . '/resources');
-
-define('WEB_PATH', ROOT_PATH . '/web'); 
-
-define('DB_PATH', ROOT_PATH . '/db'); 
+include_once __DIR__ . '/constants.php';
 
 include_once ROOT_PATH . '/vendor/autoload.php';
 

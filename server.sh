be11ae4bd5f3f73b4bfa0d821de3d55fb973dee7
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-07-30 16:44:25 -0300

    Improved built-in server
    Windows: Prevent erros if called .bat in different dir using command
    line
    Linux: Added variables for config your preferences

--- server.sh
+++ server.sh
@@ -1,1 +1,12 @@
-/usr/bin/php5.6 -S localhost:$1 -t web/
+#!/bin/bash
+
+# setup PHP
+PHP_BIN="/usr/bin/php"
+PHP_INI="/etc/php5/cli/php.ini"
+HOST_PORT=9000
+
+# Used to restore current dir if using command line
+BASEDIR=$(dirname "$0")
+
+# execute built-in server
+$PHP_BIN -S localhost:$HOST_PORT -c $PHP_INI -t "$BASEDIR/web"

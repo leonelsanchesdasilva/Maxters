96b12e74eb579849fffbbf31de0ff92105ab4bb8
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-07-31 20:07:10 -0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -5,7 +5,7 @@ PHP_BIN="/usr/bin/php"
 PHP_INI="/etc/php5/cli/php.ini"
 HOST_PORT=9000
 
-# Used to restore current dir if using command line
+# Get script path location
 BASEDIR=$(dirname "$0")
 
 # execute built-in server
